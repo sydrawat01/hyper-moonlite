f2b860a6d6c0b4f89632573ca9c97163b334f959
Siddharth Rawat <siddharth.rawat94@gmail.com>
2019-07-23 00:50:13 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# hyper-moonlite
+
+The hyper-moonlite is a spin-off of [new moon syntax theme](https://github.com/taniarascia/new-moon) for Hyper.
+
+[![PRs Welcome][prs-badge]][prs]
+
+## ↓ Install
+
+Installing the hyper-moonlite theme for your Hyper terminal couldn't be easier.
+
+### npm
+
+1.  `npm i hyper-moonlite`
+
+### Hyper's built-in CLI (hyper)
+
+1.  `hyper i hyperterm-new-moon-theme`
+
+### Manually
+
+1.  Open `~/.hyper.js` in your editor.
+2.  Add `hyperterm-new-moon-theme` to the `plugins` array.
+
+## ↩︎ Contribute
+
+If you would like to help improve this theme, you're more than welcome to contribute.
+
+[prs-badge]: https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square
+[prs]: http://makeapullrequest.com
---
+++ index.js
@@ -0,0 +1,64 @@
+// standard ANSI Colours
+const black = '#2b303b';
+const red = '#bf616a';
+const green = '#a3be8c';
+const yellow = '#ebcb8b';
+const blue = '#8fa1b3';
+const magenta = '#b48ead';
+const cyan = '#96b5b4';
+const white = '#c0c5ce';
+const lightBlack = '#2b303b';
+const lightRed = '#bf616a';
+const lightGreen = '#a3be8c';
+const lightYellow = '#ebcb8b';
+const lightBlue = '#8fa1b3';
+const lightMagenta = '#b48ead';
+const lightCyan = '#96b5b4';
+const lightWhite = '#c0c5ce';
+
+const accentColor = blue;
+
+exports.decorateConfig = config =>
+  Object.assign({}, config, {
+    cursorColor: accentColor,
+    cursorAccentColor: lightWhite,
+    foregroundColor: white,
+    backgroundColor: black,
+    borderColor: 'rgba(255,255,255, 0.2)',
+    selectionColor: 'rgba(255,255,255, 0.2)',
+    css: `
+    ${config.css || ''}
+    .tab_tab {
+      border: 0;
+      background-color: rgba(255, 255, 255, 0.04);
+    }
+    .tab_text {
+      background-color: rgba(255, 255, 255, 0.04);;
+    }
+    .tab_active {
+      background-color: ${black};
+    }
+    .tab_textActive {
+      background-color: ${black};
+      border-top: 2px solid ${accentColor} !important;
+    }
+  `,
+    colors: {
+      black,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      white,
+      lightBlack,
+      lightRed,
+      lightGreen,
+      lightYellow,
+      lightBlue,
+      lightMagenta,
+      lightCyan,
+      lightWhite
+    }
+  });
